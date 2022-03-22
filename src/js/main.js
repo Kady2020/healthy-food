34af277c7338cd7db70377cb2d45eda3e3e37e2a
Kady <74194999+Kady2020@users.noreply.github.com>
2022-03-22 14:04:49 +0300

    added fonts

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,9 @@ import * as flsFunctions from "./modules/webp.js";
 
 flsFunctions.isWebp();
 
-// import "./";
+import "./libraries/jquery-3.6.0.min.js";
+import "./libraries/slick.min.js";
+import "./modules/header-scroll.js";
+import "./modules/burger-menu.js";
+import "./modules/slick-slider.js";
+
